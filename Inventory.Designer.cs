93d44a578fb6bcb983dbe589396ca9cb3021e032
Matt Brown <tcisbrowm@tuscolacounty.org>
2021-10-21 16:25:52 -0400

    Removed add User

--- Inventory.Designer.cs
+++ Inventory.Designer.cs
@@ -34,7 +34,6 @@
             this.label7 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.addUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.totalValueReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -139,18 +138,11 @@
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.addUserToolStripMenuItem,
             this.toolStripMenuItem2,
             this.exirtToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(41, 24);
             this.toolStripMenuItem1.Text = "File";
-            // 
-            // addUserToolStripMenuItem
-            // 
-            this.addUserToolStripMenuItem.Name = "addUserToolStripMenuItem";
-            this.addUserToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
-            this.addUserToolStripMenuItem.Text = "AddUser";
             // 
             // toolStripMenuItem2
             // 
@@ -158,27 +150,27 @@
             this.toolStripMenuItem3,
             this.totalValueReportToolStripMenuItem});
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(128, 22);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
             this.toolStripMenuItem2.Text = "Reports";
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(187, 22);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
             this.toolStripMenuItem3.Text = "Inventory Report";
             this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
             // 
             // totalValueReportToolStripMenuItem
             // 
             this.totalValueReportToolStripMenuItem.Name = "totalValueReportToolStripMenuItem";
-            this.totalValueReportToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
-            this.totalValueReportToolStripMenuItem.Text = "Total Value Report";
+            this.totalValueReportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.totalValueReportToolStripMenuItem.Text = "Total Value ";
             this.totalValueReportToolStripMenuItem.Click += new System.EventHandler(this.totalValueReportToolStripMenuItem_Click);
             // 
             // exirtToolStripMenuItem
             // 
             this.exirtToolStripMenuItem.Name = "exirtToolStripMenuItem";
-            this.exirtToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.exirtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exirtToolStripMenuItem.Text = "Exit";
             this.exirtToolStripMenuItem.Click += new System.EventHandler(this.exirtToolStripMenuItem_Click);
             // 
@@ -194,14 +186,14 @@
             // aboutToolStripMenuItem1
             // 
             this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
-            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(115, 22);
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.aboutToolStripMenuItem1.Text = "About";
             this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
             // 
             // bonusToolStripMenuItem
             // 
             this.bonusToolStripMenuItem.Name = "bonusToolStripMenuItem";
-            this.bonusToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.bonusToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.bonusToolStripMenuItem.Text = "Bonus!";
             this.bonusToolStripMenuItem.Click += new System.EventHandler(this.bonusToolStripMenuItem_Click);
             // 
@@ -477,7 +469,7 @@
             "7",
             "8",
             "9"});
-            this.comboBox3.Location = new System.Drawing.Point(91, 10);
+            this.comboBox3.Location = new System.Drawing.Point(91, 7);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(83, 26);
             this.comboBox3.TabIndex = 1;
@@ -494,7 +486,7 @@
             "E",
             "F",
             "G"});
-            this.comboBox2.Location = new System.Drawing.Point(3, 10);
+            this.comboBox2.Location = new System.Drawing.Point(3, 7);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(82, 26);
             this.comboBox2.TabIndex = 0;
@@ -628,7 +620,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem addUserToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exirtToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
